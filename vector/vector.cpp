23f52672b4c8e525c5e0bebb4d88621e27678527
zfl990109 <1491657576@qq.com>
2021-12-28 16:23:06 +0800

    data structure and algorithm

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -3,3 +3,141 @@
 //
 
 #include "vector.h"
+#include <iostream>
+using namespace std;
+//数组复制
+template<typename T>
+void vector<T>::copyFrom(const T *A, int lo, int hi) {
+    _ele = new T[_capacity = 2 * (hi - lo)];
+    _size = 0;
+    while(lo < hi){
+        _ele[_size++] = A[lo++];
+    }
+}
+//扩容
+template<typename T>
+void vector<T>::expand() {
+    if (_size < _capacity) return;
+    if (_capacity < DEFAULT_CAPACITY) _capacity = DEFAULT_CAPACITY;
+    T* oldEle = _ele;
+    _ele = new T[_capacity <<= 1];
+    for (int i = 0; i < _size; ++i) {
+        _ele[i] = oldEle[i];
+    }
+    delete []oldEle;
+}
+//收缩
+template<typename T>
+void vector<T>::shrink() {
+    if (_capacity < DEFAULT_CAPACITY << 1) return;
+    if (_size << 2 > _capacity) return;
+    T* oldEle = _ele;
+    _ele = new T[_capacity >>= 1];
+    for (int i = 0; i < _size; ++i) {
+        _ele[i] = oldEle[i];
+    }
+    delete []oldEle;
+}
+//无序向量查找
+template<typename T>
+int vector<T>::find(const T &e, int lo, int hi) const {
+    while((lo < hi--) && (e != _ele[hi]));
+    return hi;
+}
+//有序向量查找
+//TODO:实现binary_search和fib_search
+template<typename T>
+int vector<T>::search(const T &e, int lo, int hi) const {
+    return 0;
+}
+//随机访问
+template<typename T>
+T &vector<T>::operator[](int r) const {
+    return _ele[r];
+}
+//向量赋值
+template<typename T>
+vector<T> &vector<T>::operator=(const vector<T> &v) {
+    if (_ele) delete []_ele;
+    copyFrom(v._ele, 0, v.size());
+    return *this;
+}
+//插入元素
+template<typename T>
+int vector<T>::insert(int r, const T &e) {
+    expand();
+    for (int i = _size; i > r ; i--) {
+        _ele[i] = _ele[i - 1];
+    }
+    _ele[r] = e;
+    _size++;
+    return r;
+}
+//区间删除
+template<typename T>
+int vector<T>::remove(int lo, int hi) {
+    if (lo == hi) return 0;
+    while (hi < _size) _ele[lo++] = _ele[hi++];
+    _size = lo;
+    shrink();
+    return hi - lo;
+}
+//单个删除
+template<typename T>
+T vector<T>::remove(int r) {
+    T e = _ele[r];
+    remove(r, r + 1);
+    return e;
+}
+//置乱器
+template<typename T> void permute(vector<T>& v){
+    for (int i = v.size(); i > 0; i--) {
+        swap(v[i - 1], v[rand() % i]);
+    }
+}
+//向量整体置乱
+template<typename T>
+void vector<T>::unsort(int lo, int hi) {
+    T* v = _ele + lo;
+    for (int i = hi - lo; i > 0; i--) {
+        swap(v[i - 1], v[rand() % i]);
+    }
+}
+//无序向量去重
+template<typename T>
+int vector<T>::deduplicate() {
+    int old_size = _size;
+    int i = 1;
+    while (i < _size){
+        (find(_ele[i], 0, i) < 0) ? i++ : remove(i);
+    }
+    return old_size - _size;
+}
+//有序向量去重
+template<typename T>
+int vector<T>::uniqufy() {
+    int i = 0, j = 0;
+    while (++j <_size) {
+        if (_ele[i] != _ele[j])
+            _ele[++i] = _ele[j];
+    }
+    _size = ++i;
+    shrink();
+    return j - i;
+}
+//函数指针遍历
+template<typename T>
+void vector<T>::traverse(void (*visit)(T &)) {
+    for (int i = 0; i < _size; ++i) {
+        visit(_ele[i]);
+    }
+}
+//函数对象遍历
+template<typename T>
+template<typename vst>
+void vector<T>::traverse(vst & visit) {
+    for (int i = 0; i < _size; ++i) {
+        visit(_ele[i]);
+    }
+}
+
--- vector/vector.h
+++ vector/vector.h
@@ -5,14 +5,55 @@
 #ifndef DSA_VECTOR_H
 #define DSA_VECTOR_H
 #define DEFAULT_CAPACITY 5
+
 template<typename T>
 class vector {
 protected:
-    int _size;
-    int _capacity;
+    int _size{};
+    int _capacity{};
     T* _ele;
+    void copyFrom(const T* A, int lo, int hi);
+    void expand();
+    void shrink();
+
+public:
+    explicit vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0){   //构造函数
+        _ele = new T[_capacity = c];
+        for(_size = 0; _size < s; _size++){
+            _ele[_size] = v;
+        }
+    }
+    vector(const T* A, int n){ copyFrom(A, 0, n); }     //数组整体复制
+    vector(const T* A, int lo, int hi){ copyFrom(A, lo, hi); }     //数组区间复制
+    vector(vector<T> const& v){ copyFrom(v._ele, 0, v._size); }     //向量整体复制
+    vector(vector<T> const& v, int lo, int hi){ copyFrom(v._ele, lo, hi); }     //向量区间复制
+    ~vector(){ delete []_ele; }     //析构函数
+    /***********************只读访问接口***********************************************/
+    int size() const{ return _size; }
+    bool empty() const{ return !_size; }
+    int find(const T& e) const { return find(e, 0, _size); }    //无序向量整体查找
+    int find(const T& e, int lo, int hi) const;     //无序向量区间查找
+    int search(const T& e) const{   //有序向量整体查找
+        return (_size <= 0) ? -1 : search(e, 0 , _size);
+    }
+    int search(const T& e, int lo, int hi) const;   //有序向量区间查找
+
+    /*********************可写访问接口******************************************/
+    T& operator[](int r) const;     //操作符重载,类似数组访问
+    vector<T>& operator=(const vector<T>&);     //重载赋值操作符，以便直接克隆向量
+    T remove(int r);    //删除单个元素,返回删除元素的值
+    int remove(int lo, int hi);     //删除区间,返回删除元素的个数
+    int insert(int r, const T& e);      //插入元素
+    int insert(const T& e) { return insert(_size, e); }     //作为末尾元素插入
+    void sort(int lo, int hi);      //排序
+    void unsort(int lo, int hi);    //置乱
+    int deduplicate();      //无序向量去重
+    int uniqufy();      //有序向量去重
+
+    /************遍历***********************************************************/
+    void traverse( void(*) (T&) );      //使用函数指针遍历,只读或局部性修改
+    template<typename vst> void traverse( vst& );      //使用函数对象遍历,可全局性修改
 
 };
-
 
 #endif //DSA_VECTOR_H
