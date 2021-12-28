94c8bd6dbdf578d7e204c0c7d28c9ed0ae9c2697
zfl990109 <1491657576@qq.com>
2021-12-28 10:39:01 +0800

    test

---
+++ vector/vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zfl on 2021/12/28.
+//
+
+#include "vector.h"
---
+++ vector/vector.h
@@ -0,0 +1,18 @@
+//
+// Created by zfl on 2021/12/28.
+//
+
+#ifndef DSA_VECTOR_H
+#define DSA_VECTOR_H
+#define DEFAULT_CAPACITY 5
+template<typename T>
+class vector {
+protected:
+    int _size;
+    int _capacity;
+    T* _ele;
+
+};
+
+
+#endif //DSA_VECTOR_H
